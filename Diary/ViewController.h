db7ea4f00365028f8a3b1933521f9132d0840b76
Noah-T <Nteshu@gmail.com>
2014-10-04 19:54:44 -0400

    Initial Commit

---
+++ Diary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Diary
+//
+//  Created by Noah Teshu on 10/4/14.
+//  Copyright (c) 2014 Noah Teshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
